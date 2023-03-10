7e49df2e8bf1523b8cd967aa2681ab9629f6e8a0
Luciano Marani <lmarani@santafe.gov.ar>
2023-03-10 15:28:25 -0300

    4.4

--- bloglist/utils/list_helper.js
+++ bloglist/utils/list_helper.js
@@ -2,6 +2,16 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  const reducer = (sum, blog) => {
+    return sum + blog.likes
+  }
+
+  console.log('loooog', blogs === 0 ? 0 : blogs.reduce(reducer, 0))
+  return blogs === 0 ? 0 : blogs.reduce(reducer, 0)
+}
+
 module.exports = {
   dummy,
+  totalLikes,
 }
