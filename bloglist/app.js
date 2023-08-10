78b60ab5db1d29ada2fb3ae9b79479162a012b1f
Luciano Marani <lmarani@santafe.gov.ar>
2023-08-10 09:47:59 -0300

    Ex 4.15

--- bloglist/app.js
+++ bloglist/app.js
@@ -3,7 +3,10 @@ const express = require('express')
 const app = express()
 require('express-async-errors')
 const cors = require('cors')
+
 const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
+
 const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
@@ -27,6 +30,7 @@ app.use(express.json())
 app.use(middleware.requestLogger)
 
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
